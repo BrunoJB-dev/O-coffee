7e9d16fdfbd0a26cab36a1b895575ea9915ae768
BrunoJB <bruno.jean-baptiste@oclock.school>
2024-07-30 12:20:44 +0200

    Mise en place de la BDD

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const router = require('./app/router');
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', 'app/views');
+app.set('views', path.join(__dirname, 'app', 'views'));
 
 app.use(express.static('public'));
 
@@ -14,3 +14,7 @@ app.use(express.static('public'));
 
 app.use(router);
 
+const PORT = process.env.PORT || 3300;
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`);
+});
